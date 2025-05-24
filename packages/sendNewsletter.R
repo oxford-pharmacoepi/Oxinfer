6a3a2c9ff7f08126f51af204a4797a882d19d0fd
catalamarti <marticatalasabate@gmail.com>
2025-05-24 11:35:40 +0100

    Update sendNewsletter.R

--- packages/sendNewsletter.R
+++ packages/sendNewsletter.R
@@ -1,22 +1,15 @@
-library(lubridate)
-library(blastula)
-library(dplyr)
-library(readr)
-library(here)
-library(stringr)
-
-source(here("packages", "scrapePackages.R"))
+source(here::here("packages", "scrapePackages.R"))
 
 period <- format(Sys.Date() - 1, "%B %Y")
 
 # Generate newsletter content
-newsletter <- read_csv(here("packages", "packages.csv"), show_col_types = FALSE) |>
-  filter(dev == "ox") |>
-  select(!dev) |>
+newsletter <- readr::read_csv(here::here("packages", "packages.csv"), show_col_types = FALSE) |>
+  dplyr::filter(dev == "ox") |>
+  dplyr::select(!dev) |>
   createNewsletter(period = period)
 
 # Strip title
-body_md <- str_replace(newsletter, pattern = paste0("## ", period, "\n\n"), replacement = "")
+body_md <- stringr::str_replace(newsletter, pattern = paste0("## ", period, "\n\n"), replacement = "")
 
 # Load recipients
 url <- "https://docs.google.com/spreadsheets/d/1FhUkmLhAvOFlqK54hJRzmZocGF1MS8hTLkn9Fpr1bSw/export?format=csv"
@@ -31,7 +24,8 @@ email_creds <- blastula::creds_envvar(
   pass_envvar = "OXINFER_GMAIL",
   host = "smtp.gmail.com",
   port = 587,
-  use_ssl = TRUE
+  use_ssl = FALSE,
+  use_tls = TRUE 
 )
 
 # Loop through and send
@@ -48,24 +42,24 @@ for (email in emails) {
     "Thank you for your continued support. If you have feedback, contributions, or suggestions, we'd love to hear from you.\n\n",
     "Until next time,\n\n",
     "The **Oxinfer Team**\n\n",
-    "[https://oxford-pharmacoepi.github.io/Oxinfer/](https://oxford-pharmacoepi.github.io/Oxinfer/)\n\n",
-    "---\n\n",
-    "If you’d like to stop receiving this newsletter, you can [unsubscribe here](", unsubscribe, ")."
+    "[https://oxford-pharmacoepi.github.io/Oxinfer/](https://oxford-pharmacoepi.github.io/Oxinfer/)"
   )
   
   # Compose the message
-  email_msg <- compose_email(
-    body = md(body_content)
+  email_msg <- blastula::compose_email(
+    body = blastula::md(body_content), 
+    footer = blastula::md(paste0("If you’d like to stop receiving this newsletter, you can [unsubscribe here](", unsubscribe, ")."))
   )
   
   # Send the email
   tryCatch({
-    smtp_send(
+    blastula::smtp_send(
       email = email_msg,
       from = "oxinfer@gmail.com",
       to = email,
       subject = subject,
-      credentials = email_creds
+      credentials = email_creds, 
+      verbose = TRUE
     )
   }, error = function(e) {
     message(sprintf("Failed to send to %s: %s", email, e$message))
