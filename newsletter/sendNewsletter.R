05073e5b2f4fe16fddbb0772126388f52c712b17
catalamarti <marticatalasabate@gmail.com>
2025-05-26 12:34:51 +0100

    Create newsletter separate folder

--- newsletter/sendNewsletter.R
+++ newsletter/sendNewsletter.R
@@ -2,7 +2,7 @@
 renv::status()
 renv::restore()
 
-source(here::here("scrapePackages.R"))
+source(here::here("createNewsletter.R"))
 
 period <- format(Sys.Date() - 1, "%B %Y")
 
@@ -12,17 +12,31 @@ newsletter <- readr::read_csv(here::here("packages.csv"), show_col_types = FALSE
   dplyr::select(!dev) |>
   createNewsletter(period = period)
 
-# Strip title
-body_md <- stringr::str_replace(newsletter, pattern = paste0("## ", period, "\n\n"), replacement = "")
+# Create newsletter
+body_content <- paste0(
+  "Dear colleagues,\n\n",
+  "Welcome to the latest edition of the **Oxinfer Newsletter**! Here's a summary of recent updates and activity in our packages — thank you for staying engaged with our work.\n\n",
+  "---\n\n",
+  stringr::str_replace(newsletter, pattern = paste0("## ", period, "\n\n"), replacement = ""), "\n\n",
+  "---\n\n",
+  "Check our [**daily newsletter**](https://oxford-pharmacoepi.github.io/Oxinfer/packages/newsletter.html) for daily updates.\n\n",
+  "Thank you for your continued support. If you have feedback, contributions, or suggestions, we'd love to hear from you.\n\n",
+  "Until next time,\n\n",
+  "The **Oxinfer Team**\n\n",
+  "[https://oxford-pharmacoepi.github.io/Oxinfer/](https://oxford-pharmacoepi.github.io/Oxinfer/)"
+)
 
 # Load recipients
 if (identical(Sys.getenv("TEST_RUN"), "true")) {
+  # send to oxinfer email for test runs
+  emails <- "oxinfer@gmail.com"
+} else { 
   url <- "https://docs.google.com/spreadsheets/d/1FhUkmLhAvOFlqK54hJRzmZocGF1MS8hTLkn9Fpr1bSw/export?format=csv"
   df <- readr::read_csv(url, show_col_types = FALSE)
   emails <- unique(df$email)
-} else { # send to oxinfer email for test runs
-  emails <- "oxinfer@gmail.com"
 }
+
+emails <- emails[!is.na(emails)]
 
 subject <- paste0("Oxinfer Newsletter (", period, ")")
 
@@ -36,27 +50,15 @@ email_creds <- blastula::creds_envvar(
 options(blastula.verbose = TRUE)
 # Loop through and send
 for (email in emails) {
-  unsubscribe <- unsubscribeLink(email)
-  
-  body_content <- paste0(
-    "Dear colleagues,\n\n",
-    "Welcome to the latest edition of the **Oxinfer Newsletter**! Here's a summary of recent updates and activity in our packages — thank you for staying engaged with our work.\n\n",
-    "---\n\n",
-    body_md, "\n\n",
-    "---\n\n",
-    "Check our [**daily newsletter**](https://oxford-pharmacoepi.github.io/Oxinfer/packages/newsletter.html) for daily updates.\n\n",
-    "Thank you for your continued support. If you have feedback, contributions, or suggestions, we'd love to hear from you.\n\n",
-    "Until next time,\n\n",
-    "The **Oxinfer Team**\n\n",
-    "[https://oxford-pharmacoepi.github.io/Oxinfer/](https://oxford-pharmacoepi.github.io/Oxinfer/)"
-  )
   
   # Compose the message
+  unsubscribe <- unsubscribeLink(email) # unsubscribe is different for each email
   email_msg <- blastula::compose_email(
     body = blastula::md(body_content), 
     footer = blastula::md(paste0("If you’d like to stop receiving this newsletter, you can [unsubscribe here](", unsubscribe, ")."))
   )
   
+  # send email
   tryCatch({
     blastula::smtp_send(
       email = email_msg,
